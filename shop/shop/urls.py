39319982317d3fa96f23566db1184cc6c9886f51
Vlad Mitlytskyi <90638567+mvladv@users.noreply.github.com>
2023-03-28 16:53:50 +0300

    Update README.md

--- shop/shop/urls.py
+++ shop/shop/urls.py
@@ -18,5 +18,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('main.urls'))
+    path('', include('main.urls')),
+    path('news/', include('news.urls')),
+    path('store/', include('storehouse.urls')),
+    path('order/', include('order.urls')),
 ]
